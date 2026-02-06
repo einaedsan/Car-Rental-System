4edf2797010a9f21b8bbff1bc40e17d900ec5ee4
sana712 <byunsansa898@gmail.com>
2026-02-06 00:51:24 -0800

    rental storage and queue

--- CarRentalSystem/Rental.cpp
+++ CarRentalSystem/Rental.cpp
@@ -1,7 +1,8 @@
-#include "Rental.h"
+﻿#include "Rental.h"
 
 int Rental::nextId = 1;
 
+// Constructor
 Rental::Rental(int uId, int cId, int start, int expected)
     : rentalId(nextId++),
     userId(uId),
@@ -13,42 +14,24 @@ Rental::Rental(int uId, int cId, int start, int expected)
     lateFine(0) {
 }
 
-int Rental::getId() const {
-    return rentalId;
-}
+// Getter ها
+int Rental::getId() const { return rentalId; }
+int Rental::getUserId() const { return userId; }
+int Rental::getCarId() const { return carId; }
+int Rental::getStartDay() const { return startDay; }
+int Rental::getExpectedReturnDay() const { return expectedReturnDay; }
+int Rental::getActualReturnDay() const { return actualReturnDay; }
+double Rental::getTotalCost() const { return totalCost; }
+double Rental::getLateFine() const { return lateFine; }
 
-int Rental::getUserId() const {
-    return userId;
-}
-
-int Rental::getCarId() const {
-    return carId;
-}
-
-int Rental::getStartDay() const {
-    return startDay;
-}
-
-int Rental::getExpectedReturnDay() const {
-    return expectedReturnDay;
-}
-
-int Rental::getActualReturnDay() const {
-    return actualReturnDay;
-}
-
-double Rental::getTotalCost() const {
-    return totalCost;
-}
-
-double Rental::getLateFine() const {
-    return lateFine;
-}
-
+// بستن اجاره و محاسبه جریمه
 void Rental::closeRental(int actual, double dailyFine) {
     actualReturnDay = actual;
 
     if (actualReturnDay > expectedReturnDay) {
         lateFine = (actualReturnDay - expectedReturnDay) * dailyFine;
+    }
+    else {
+        lateFine = 0;
     }
 }
